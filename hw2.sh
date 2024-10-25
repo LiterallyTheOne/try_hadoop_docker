26cc6c7985e4fc3cd1b10fafd07bcefc57c3bfcb
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2024-10-25 17:01:44 +0330

    add a hw2.sh file to make my life easier

---
+++ hw2.sh
@@ -0,0 +1,6 @@
+hdfs dfs -rm -r /hw2_output
+
+yarn jar $HADOOP_STREAMING_JAR -files mapper_2.py,reducer_2.py \
+    -mapper "python mapper_2.py" \
+    \
+    -numReduceTasks 1 -input /hw2 -output /hw2_output # -reducer "python reducer_2.py" \
