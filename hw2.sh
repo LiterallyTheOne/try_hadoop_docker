8ba51e79e91871025f0d565385752c8fd60ba82f
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2024-10-25 18:57:11 +0330

    Add reducer to hw2.sh

--- hw2.sh
+++ hw2.sh
@@ -2,5 +2,5 @@ hdfs dfs -rm -r /hw2_output
 
 yarn jar $HADOOP_STREAMING_JAR -files mapper_2.py,reducer_2.py \
     -mapper "python mapper_2.py" \
-    \
-    -numReduceTasks 1 -input /hw2 -output /hw2_output # -reducer "python reducer_2.py" \
+    -reducer "python reducer_2.py" \
+    -numReduceTasks 2 -input /hw2 -output /hw2_output
