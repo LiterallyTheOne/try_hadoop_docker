e878885fa18ae9d7b592cad8179d301d1510584b
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2024-11-03 11:29:53 +0330

    Change the number of reduce tasks to 1

--- hw2.sh
+++ hw2.sh
@@ -3,4 +3,4 @@ hdfs dfs -rm -r /hw2_output
 yarn jar $HADOOP_STREAMING_JAR -files mapper_2.py,reducer_2.py \
     -mapper "python mapper_2.py" \
     -reducer "python reducer_2.py" \
-    -numReduceTasks 2 -input /hw2 -output /hw2_output
+    -numReduceTasks 1 -input /hw2 -output /hw2_output
